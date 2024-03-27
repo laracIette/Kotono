543edf618efda2b6118e4fcbc22e41b0ea317e72
laracIette <nicosavlar@gmail.com>
2024-03-27 13:18:52 +0100

    public Time

--- Kotono/Utils/Time.cs
+++ Kotono/Utils/Time.cs
@@ -2,41 +2,41 @@
 
 namespace Kotono.Utils
 {
-    internal static class Time
+    public static class Time
     {
         private static double StartTime { get; } = DateTime.UtcNow.Ticks / (double)TimeSpan.TicksPerSecond;
 
         /// <summary>
         /// Current UTC Time since Epoch in seconds.
         /// </summary>
-        internal static double SinceEpoch { get; private set; } = StartTime;
+        public static double SinceEpoch { get; private set; } = StartTime;
 
         /// <summary>
         /// Current UTC Time since Epoch in milliseconds.
         /// </summary>
-        internal static double SinceEpochMS => SinceEpoch * 1000.0;
+        public static double SinceEpochMS => SinceEpoch * 1000.0;
 
         /// <summary>
         /// Current Time since the start of the program in seconds.
         /// </summary>
-        internal static float Now { get; private set; } = 0.0f;
+        public static float Now { get; private set; } = 0.0f;
 
         /// <summary>
         /// Current Time since the start of the program in milliseconds.
         /// </summary>
-        internal static float NowMS => Now * 1000.0f;
+        public static float NowMS => Now * 1000.0f;
 
         /// <summary>
         /// Delta Time in seconds.
         /// </summary>
-        internal static float Delta { get; private set; } = 0.0f;
+        public static float Delta { get; private set; } = 0.0f;
 
         /// <summary>
         /// Delta Time in milliseconds.
         /// </summary>
-        internal static float DeltaMS => Delta * 1000.0f;
+        public static float DeltaMS => Delta * 1000.0f;
 
-        internal static void Update()
+        public static void Update()
         {
             SinceEpoch = DateTime.UtcNow.Ticks / (double)TimeSpan.TicksPerSecond;
 
